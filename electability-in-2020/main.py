735891c86552ee383ae143741dcb7ce620766e82
Josh Vieira <josh.vieira90@gmail.com>
2020-06-24 23:23:12 -0400

    new homepage and edited electability text

--- electability-in-2020/main.py
+++ electability-in-2020/main.py
@@ -10,7 +10,7 @@ import redis
 import pyarrow as pa
 
 CUTOFF = 0.15  # minimum required contract price in democratic candidate market in order to show probability
-YMAX_DEMS = 0.6
+YMAX_DEMS = 1.0
 YMAX_PRES = 1.0
 YMIN_PROB = 0.4
 YMAX_PROB = 0.9
@@ -125,7 +125,7 @@ def gen_dems():
 def gen_prob(x_range):
 
     p = figure(
-        title="Probability to win Presidency GIVEN chosen as party's nominee",
+        title="Probability of winning presidency if chosen as nominee",
         x_axis_type="datetime",
         x_range=x_range,
         plot_width=PLOT_WIDTH,
@@ -194,31 +194,31 @@ def gen_prob(x_range):
 
     return p
 
-textstyle = style={'font-size': '15px'}
+textstyle = style={'font-size': '17px'}
 textwidth = int(PLOT_WIDTH * 0.9)
 
 text0 = Div(text='<u>What might prediction markets tell us about electability?</u>', style={'font-size': '30px'})
 text1 = Div(text='29-Jan-2020', style=textstyle)
-text1_1 = Div(text='updated June 2020', style=textstyle)
+text1_1 = Div(text='Text updated June 2020', style=textstyle)
 
 text2 = Div(text="""By using data from a betting market for political events, we can get an estimate for the 
-'electability' of candidates. On <a href="predictit.com">predictit.com</a>, 
+'electability' of candidates. On <a href="https://www.predictit.com">predictit.com</a>, 
 users can bet on which candidates will win each major party's nomination, 
 and on which candidates will win the general election. By dividing the price of the latter contracts by the 
 price of the former, we get an estimate for the conditional probability 
-that a candidate wins the general election given they were chosen as their party's nominee.
+that a candidate wins the general election given they were chosen as their party's nominee. 
+This is the notion of electability. If nominated, what are the odds of beating your opponent in the general election? 
 """, width=textwidth, style=textstyle)
 
-text3 = Div(text="""The most recent data shows Sanders is the most electable major candidate -- the probability he wins 
-in November given he is the nominee is 66%. For Biden the corresponding number is 50%, and for Trump it is 53%.
+text3 = Div(text="""The charts below show the two time series respectively.
 """, width=textwidth, style=textstyle)
 
-text4 = Div(text='<u>Technical details and caveats:</u>', style=textstyle)
+text4 = Div(text='<u>Technical details, caveats, and comments:</u>', style=textstyle)
 text5 = Div(text="""
 Betting market contract data is pulled every 5 minutes using the predictit API. Daily averages of 
 contract values and their corresponding ratios are computed and rendered. To avoid spurious spikes in the chart due to 
 division by small numbers, the conditional probability is only rendered when a candidate's probability of winning the 
-party's nomination is above 15%. The conditional probability arises from a simple application of Bayes' Theorem, which 
+party's nomination is above 15%. The conditional probability arises from Bayes' Theorem, which 
 states   
 """, width=textwidth, style=textstyle)
 text6 = Div(text="P(Pres|Nom) = P(Nom|Pres) * P(Pres) / P(Nom)", width=textwidth, style=textstyle)
@@ -226,24 +226,42 @@ text7 = Div(text="""for the events Pres = {candidate wins Presidency} and Nom = 
 We are implicitly assuming that P(Nom|Pres) = 1, ie that there is no chance a candidate can win 
 the presidency without having received their major party's nomination. 
 """, width=textwidth, style=textstyle)
-text8 = Div(text="""A larger caveat of this analysis is that it does not tell us about the states of the world in which 
-a candidate is likely to be more or less successful. For instance, at the peak of her popularity Warren had a 51% 
-probability of being nominated. Once she started backtracking on Medicare-for-All her nomination probability declined, 
-as did her conditional probability of becoming president. This tells us that candidate's degree of support for Medicare-for-All 
-is important for voters in determining their relative support for Democratic candidates in both the primary and in the general 
-election, and that these probabilities will likely move should candidates publicly change their views on 
-this issue. A longer discussion of this topic is contained in 
+text8 = Div(text="""These numbers do not tell us about the states of the world in which 
+a candidate is likely to be more or less successful. We have to make those inferences ourselves.
+For instance, at the peak of her popularity Warren had a 51% probability of being nominated. 
+"Squad" members AOC, Ilan Omar, and Rashida Tlaib endorsed Sanders shortly thereafter, 
+and around the same time Warren began to waiver in her support for Medicare-for-All. 
+These were blows to her credibility as a progressive candidate. 
+Too timid in her willingness to enact institutional change for progressives, 
+but still too loud in her criticisms of those same institutions for those longing for a return to the Obama years, 
+in the end she failed to inspire two large swaths of Democratic voters. 
+Her nomination probability declined, as did her conditional probability of becoming president. 
+A longer discussion around this topic is contained in 
 <a href="http://users.nber.org/~jwolfers/Papers/Five%20Questions(NBER).pdf">Wolfers & Zitzewitz (2006)</a>, starting 
 on p.14.
 """, width=textwidth, style=textstyle)
-
+text9 = Div(text="""
+Betting limits also prevent Predictit from being an ideal market. 
+They limit the number of people who can be bothered to play the game, 
+and they make it much easier to buy cheap shares than to sell them short. 
+As two experts commented on Twitter,
+""", width=textwidth, style=textstyle)
+text10 = Div(text="<img src='electability-in-2020/static/twitter_natesilver.png'>")
+text11 = Div(text="""
+There are other interesting uses of this data. For instance, one could regress Trump's changes in 
+election probability against changes in each Democratic candidate's nomination probability. This would again give us 
+a sense of "electability", quantifying who has the highest impact on Trump's election odds. I may look at this in a later post. 
+If you have comments or are interested in the underlying data, email me at josh.[my last name]@protonmail.com.
+""", width=textwidth, style=textstyle)
 
 dems = gen_dems()
 prob = gen_prob(dems.x_range)
 layout = row(Spacer(width=40),
 column(
+    Spacer(height=25),
     text0,
     text1,
+    text1_1,
     text2,
     text3,
     Spacer(height=25),
@@ -255,7 +273,10 @@ column(
     text6,
     text7,
     text8,
-    Spacer(height=60)
+    text9,
+    text10,
+    text11,
+    Spacer(height=120)
 ))
 doc.add_root(layout)
 doc.title = 'Estimating electability from betting markets'
