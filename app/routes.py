d3ca15d6b56d7a9a3e2dacb6f69e7cb7f5d6df87
Josh Vieira <josh.vieira@protonmail.com>
2022-09-07 15:49:30 -0400

    kelly criterion post

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/optimal-betting-when-odds-are-random")
+def hello():
+    return render_template("optimal-betting-when-odds-are-random.html")
