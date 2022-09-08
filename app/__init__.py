5e5d086bf80c401e1c6393f2c14957d3a4763c01
Josh Vieira <josh.vieira@protonmail.com>
2022-09-08 10:52:49 -0400

    flask security

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,13 @@
-from flask import Flask
+import os
 
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
 
 from app import routes
 
+
+if os.getenv("ENV") == "prod":
+    app.config.update(SECRET_KEY=os.getenv("FLASK_SECRET_KEY"))
