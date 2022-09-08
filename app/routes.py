377794fadb09923c48e153e5585b214c68ef10db
Josh Vieira <josh.vieira@protonmail.com>
2022-09-08 16:59:01 -0400

    new home page with images

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @app.route("/optimal-betting-when-odds-are-random")
