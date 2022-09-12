27fc68253e40c33fad140df185c23eba95c39bbb
Josh Vieira <josh.vieira@protonmail.com>
2022-09-12 14:46:15 -0400

    about page

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,11 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/optimal-betting-when-odds-are-random")
 def kelly_betting():
     return render_template("optimal-betting-when-odds-are-random.html")
